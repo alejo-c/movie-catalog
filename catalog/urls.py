7df39dd1e61b9a5127d50b7ebbdac3ed49fe6468
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-03-06 18:17:56 -0500

    Set application views and urls

---
+++ catalog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("catalog", views.catalog, name="catalog"),
+    path("catalog/add", views.add, name="add"),
+    path("catalog/edit/<int:movie_id>", views.edit, name="edit"),
+    path("catalog/remove/<int:movie_id>", views.remove, name="remove"),
+    path("logout/", views.logout_view, name="logout"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ catalog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+from .models import Movie
+from .forms import MovieForm
+
+
+def index(req):
+    return render(req, "movies/index.html")
+
+
+def catalog(req):
+    movies = Movie.objects.all()
+    return render(req, "movies/movies.html", {"movies": movies})
+
+
+@login_required
+def add(req):
+    form = MovieForm(req.POST or None, req.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect("catalog")
+    return render(req, "movies/add.html", {"form": form})
+
+
+@login_required
+def edit(req, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    form = MovieForm(req.POST or None, req.FILES or None, instance=movie)
+    if form.is_valid():
+        form.save()
+        return redirect("catalog")
+    return render(req, "movies/edit.html", {"form": form})
+
+
+@login_required
+def remove(req, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    movie.delete()
+    return redirect("catalog")
+
+
+@login_required
+def logout_view(req):
+    logout(req)
+    return render(req, "auth/logout.html")
